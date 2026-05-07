4aeb33c346c5e001eedd7f11dd33f7a7ca75d9fb
saltylitchee <saltylitchee@gmail.com>
2026-05-08 08:13:07 +0900

    Fix: Remove old file and keep new file name

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+a = 49
+b = -280
+c = -4200
+
+content = (b**2 + (-4)*a*c)
+root = math.sqrt(content)
+
+x_one = (-b + root) / 2*a
+x_two = (-b - root) / 2*a
+
+print(f'{49*16-280*8-4200*4+400*2+10000}')
